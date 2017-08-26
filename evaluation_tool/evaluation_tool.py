546c6c0c508ae0654addc9f84be6a9bc1714274a
luke_sahula <lukas.sahula@icloud.com>
2017-08-26 14:01:55 +0200

    Refactor, switch read_data and compute_stats to private
    Refactor tests, refactor read_data and compute_stats to be cleaner.
    Add labels attribute to evaluation tool.

--- evaluation_tool/evaluation_tool.py
+++ evaluation_tool/evaluation_tool.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from sklearn.metrics import confusion_matrix
 
 import pandas as pd
@@ -6,69 +8,73 @@ import numpy as np
 class EvaluationTool():
 
     def __init__(self, file_path, delimiter):
+        self.true = None
+        self.pred = None
+        self.stats = None
+        self.labels = None
 
-        self.true, self.pred = self.read_data(file_path, delimiter)
-        self.stats = self.compute_stats()
+        self.__read_data(file_path, delimiter)
+        self.__compute_stats()
 
 
-    def read_data(self, file_path, delimiter):
-        labels = ['true', 'pred']
-        df = pd.read_csv(file_path, delimiter, header=None, names=labels)
-        return (df['true'], df['pred'])
+    def __read_data(self, file_path, delimiter):
+        """
+        Reads true/pred data from a file and saves it to dataframes.
+        :param file_path: Path to the file
+        :param delimiter: Symbol or as tring by which the data is delimited.
+        """
+        columns = ['true', 'pred']
+        df = pd.read_csv(file_path, delimiter, header=None, names=columns)
+        self.true = df['true']
+        self.pred = df['pred']
 
-    def compute_stats(self):
-        labels = set(self.true)
-        labels.union(self.pred)
+    def __compute_stats(self):
+        """
+        Computes TPs, FPs, TNs and FNs of the given data.
+        """
+        self.labels = sorted(set(self.true) | set(self.pred))
 
-        labels = list(labels)
-        labels.sort()
-
-        matrix = confusion_matrix(self.true, self.pred, labels=labels)
+        matrix = confusion_matrix(self.true, self.pred, labels=self.labels)
         FP = matrix.sum(axis=0) - np.diag(matrix)
         FN = matrix.sum(axis=1) - np.diag(matrix)
         TP = np.diag(matrix)
         TN = matrix.sum() - (FP + FN + TP)
 
-        stats = {}
 
-        indexes = range(0, len(matrix[0]))
+        stats = defaultdict(dict)
 
-        for label, index in zip(labels, indexes):
-            stats[label] = {}
-            stats[label]['FP'] = FP[index]
-            stats[label]['FN'] = FN[index]
-            stats[label]['TP'] = TP[index]
-            stats[label]['TN'] = TN[index]
+        for i, label in zip(range(len(self.labels)), self.labels):
+            stats[label]['FP'] = FP[i]
+            stats[label]['FN'] = FN[i]
+            stats[label]['TP'] = TP[i]
+            stats[label]['TN'] = TN[i]
 
-        return stats
+        self.stats = stats
 
     def compute_precision(self, class_label):
-        '''
+        """
         Computes precision for the given class label.
         :param class_label: Class label of the row.
         :return: Computed precision of the classifier for the given class.
-        '''
-        true_positive = self.stats[class_label]['TP']
-        false_positive = self.stats[class_label]['FP']
+        """
+        TP = self.stats[class_label]['TP']
+        FP = self.stats[class_label]['FP']
 
         try:
-            precision = true_positive / (true_positive + false_positive)
-            return precision
+            return TP / (TP + FP)
         except ZeroDivisionError:
-            print("Division by zero.")
+            print('Division by zero.')
 
     def compute_recall(self, class_label):
-        '''
+        """
         Computes recall for the given class label.
         :param class_label: Class label of the row.
         :return: Computed recall of the classifier for the given class.
-        '''
-        true_positive = self.stats[class_label]['TP']
-        false_negative = self.stats[class_label]['FN']
+        """
+        TP = self.stats[class_label]['TP']
+        FN = self.stats[class_label]['FN']
 
         try:
-            recall = true_positive / (true_positive + false_negative)
-            return recall
+            return TP / (TP + FN)
         except ZeroDivisionError:
-            pass
-        print("Exception by zero.")
+            print('Exception by zero.')
--- evaluation_tool/tests/test_evaluation_tool.py
+++ evaluation_tool/tests/test_evaluation_tool.py
@@ -11,53 +11,49 @@ class TestEvaluationTool(object):
 
     def test_read_data_minimal(self):
         file_path = 'datasets/tests/min_example'
-        evaluation_tool = EvaluationTool(file_path, ';')
+        eval_tool = EvaluationTool(file_path, ';')
 
-        assert len(evaluation_tool.true) == 15
-        assert len(evaluation_tool.pred) == 15
-        assert evaluation_tool.true.value_counts()[0] == 5
-        assert evaluation_tool.true.value_counts()[1] == 7
-        assert evaluation_tool.true.value_counts()[2] == 3
-        assert evaluation_tool.pred.value_counts()[0] == 3
-        assert evaluation_tool.pred.value_counts()[1] == 8
-        assert evaluation_tool.pred.value_counts()[2] == 4
+        assert len(eval_tool.true) == 15
+        assert len(eval_tool.pred) == 15
+        assert eval_tool.true.value_counts()[0] == 5
+        assert eval_tool.true.value_counts()[1] == 7
+        assert eval_tool.true.value_counts()[2] == 3
+        assert eval_tool.pred.value_counts()[0] == 3
+        assert eval_tool.pred.value_counts()[1] == 8
+        assert eval_tool.pred.value_counts()[2] == 4
 
     def test_compute_stats_minimal(self):
         file_path = 'datasets/tests/min_example'
-        evaluation_tool = EvaluationTool(file_path, ';')
+        eval_tool = EvaluationTool(file_path, ';')
 
-        assert len(evaluation_tool.stats) == 3
+        assert len(eval_tool.stats) == 3
 
-        assert evaluation_tool.stats[0]['TP'] == 1
-        assert evaluation_tool.stats[0]['TN'] == 8
-        assert evaluation_tool.stats[0]['FP'] == 2
-        assert evaluation_tool.stats[0]['FN'] == 4
+        assert eval_tool.stats[0]['TP'] == 1
+        assert eval_tool.stats[0]['TN'] == 8
+        assert eval_tool.stats[0]['FP'] == 2
+        assert eval_tool.stats[0]['FN'] == 4
 
-        assert evaluation_tool.stats[1]['TP'] == 5
-        assert evaluation_tool.stats[1]['TN'] == 5
-        assert evaluation_tool.stats[1]['FP'] == 3
-        assert evaluation_tool.stats[1]['FN'] == 2
+        assert eval_tool.stats[1]['TP'] == 5
+        assert eval_tool.stats[1]['TN'] == 5
+        assert eval_tool.stats[1]['FP'] == 3
+        assert eval_tool.stats[1]['FN'] == 2
 
-        assert evaluation_tool.stats[2]['TP'] == 1
-        assert evaluation_tool.stats[2]['TN'] == 9
-        assert evaluation_tool.stats[2]['FP'] == 3
-        assert evaluation_tool.stats[2]['FN'] == 2
+        assert eval_tool.stats[2]['TP'] == 1
+        assert eval_tool.stats[2]['TN'] == 9
+        assert eval_tool.stats[2]['FP'] == 3
+        assert eval_tool.stats[2]['FN'] == 2
 
     def test_compute_precision_minimal(self):
         file_path = 'datasets/tests/min_example'
-        evaluation_tool = EvaluationTool(file_path, ';')
+        eval_tool = EvaluationTool(file_path, ';')
 
 
-        prec = [
-            evaluation_tool.compute_precision(0),
-            evaluation_tool.compute_precision(1),
-            evaluation_tool.compute_precision(2)
-        ]
+        prec = [eval_tool.compute_precision(x) for x in eval_tool.labels]
 
         prec_sklearn = list(precision_score(
-            y_true=evaluation_tool.true,
-            y_pred=evaluation_tool.pred,
-            labels=[0, 1, 2],
+            y_true=eval_tool.true,
+            y_pred=eval_tool.pred,
+            labels=eval_tool.labels,
             average=None
         ))
 
@@ -65,19 +61,15 @@ class TestEvaluationTool(object):
 
     def test_compute_recall_minimal(self):
         file_path = 'datasets/tests/min_example'
-        evaluation_tool = EvaluationTool(file_path, ';')
+        eval_tool = EvaluationTool(file_path, ';')
 
 
-        recall = [
-            evaluation_tool.compute_recall(0),
-            evaluation_tool.compute_recall(1),
-            evaluation_tool.compute_recall(2)
-        ]
+        recall = [eval_tool.compute_recall(x) for x in eval_tool.labels]
 
         recall_sklearn = list(recall_score(
-            y_true=evaluation_tool.true,
-            y_pred=evaluation_tool.pred,
-            labels=[0, 1, 2],
+            y_true=eval_tool.true,
+            y_pred=eval_tool.pred,
+            labels=eval_tool.labels,
             average=None
         ))
 
@@ -87,53 +79,49 @@ class TestEvaluationTool(object):
 
     def test_read_data_minimal_string(self):
         file_path = 'datasets/tests/min_example_strings'
-        evaluation_tool = EvaluationTool(file_path, ';')
+        eval_tool = EvaluationTool(file_path, ';')
 
-        assert len(evaluation_tool.true) == 15
-        assert len(evaluation_tool.pred) == 15
-        assert evaluation_tool.true.value_counts()['a'] == 5
-        assert evaluation_tool.true.value_counts()['b'] == 7
-        assert evaluation_tool.true.value_counts()['c'] == 3
-        assert evaluation_tool.pred.value_counts()['a'] == 3
-        assert evaluation_tool.pred.value_counts()['b'] == 8
-        assert evaluation_tool.pred.value_counts()['c'] == 4
+        assert len(eval_tool.true) == 15
+        assert len(eval_tool.pred) == 15
+        assert eval_tool.true.value_counts()['a'] == 5
+        assert eval_tool.true.value_counts()['b'] == 7
+        assert eval_tool.true.value_counts()['c'] == 3
+        assert eval_tool.pred.value_counts()['a'] == 3
+        assert eval_tool.pred.value_counts()['b'] == 8
+        assert eval_tool.pred.value_counts()['c'] == 4
 
     def test_compute_stats_minimal_string(self):
         file_path = 'datasets/tests/min_example_strings'
-        evaluation_tool = EvaluationTool(file_path, ';')
+        eval_tool = EvaluationTool(file_path, ';')
 
-        assert len(evaluation_tool.stats) == 3
+        assert len(eval_tool.stats) == 3
 
-        assert evaluation_tool.stats['a']['TP'] == 1
-        assert evaluation_tool.stats['a']['TN'] == 8
-        assert evaluation_tool.stats['a']['FP'] == 2
-        assert evaluation_tool.stats['a']['FN'] == 4
+        assert eval_tool.stats['a']['TP'] == 1
+        assert eval_tool.stats['a']['TN'] == 8
+        assert eval_tool.stats['a']['FP'] == 2
+        assert eval_tool.stats['a']['FN'] == 4
 
-        assert evaluation_tool.stats['b']['TP'] == 5
-        assert evaluation_tool.stats['b']['TN'] == 5
-        assert evaluation_tool.stats['b']['FP'] == 3
-        assert evaluation_tool.stats['b']['FN'] == 2
+        assert eval_tool.stats['b']['TP'] == 5
+        assert eval_tool.stats['b']['TN'] == 5
+        assert eval_tool.stats['b']['FP'] == 3
+        assert eval_tool.stats['b']['FN'] == 2
 
-        assert evaluation_tool.stats['c']['TP'] == 1
-        assert evaluation_tool.stats['c']['TN'] == 9
-        assert evaluation_tool.stats['c']['FP'] == 3
-        assert evaluation_tool.stats['c']['FN'] == 2
+        assert eval_tool.stats['c']['TP'] == 1
+        assert eval_tool.stats['c']['TN'] == 9
+        assert eval_tool.stats['c']['FP'] == 3
+        assert eval_tool.stats['c']['FN'] == 2
 
     def test_compute_precision_minimal_string(self):
         file_path = 'datasets/tests/min_example_strings'
-        evaluation_tool = EvaluationTool(file_path, ';')
+        eval_tool = EvaluationTool(file_path, ';')
 
 
-        prec = [
-            evaluation_tool.compute_precision('a'),
-            evaluation_tool.compute_precision('b'),
-            evaluation_tool.compute_precision('c')
-        ]
+        prec = [eval_tool.compute_precision(x) for x in eval_tool.labels]
 
         prec_sklearn = list(precision_score(
-            y_true=evaluation_tool.true,
-            y_pred=evaluation_tool.pred,
-            labels=['a', 'b', 'c'],
+            y_true=eval_tool.true,
+            y_pred=eval_tool.pred,
+            labels=eval_tool.labels,
             average=None
         ))
 
@@ -141,19 +129,15 @@ class TestEvaluationTool(object):
 
     def test_compute_recall_minimal_string(self):
         file_path = 'datasets/tests/min_example_strings'
-        evaluation_tool = EvaluationTool(file_path, ';')
+        eval_tool = EvaluationTool(file_path, ';')
 
 
-        recall = [
-            evaluation_tool.compute_recall('a'),
-            evaluation_tool.compute_recall('b'),
-            evaluation_tool.compute_recall('c')
-        ]
+        recall = [eval_tool.compute_recall(x) for x in eval_tool.labels]
 
         recall_sklearn = list(recall_score(
-            y_true=evaluation_tool.true,
-            y_pred=evaluation_tool.pred,
-            labels=['a', 'b', 'c'],
+            y_true=eval_tool.true,
+            y_pred=eval_tool.pred,
+            labels=eval_tool.labels,
             average=None
         ))
 
